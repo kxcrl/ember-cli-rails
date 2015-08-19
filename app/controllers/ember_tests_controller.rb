c3b002965390ae26f2225657596684eda20d956e
kxcrl <jeramy.couts@gmail.com>
2015-08-19 09:59:24 -0700

    Add compiled rails assets to test controller html

--- app/controllers/ember_tests_controller.rb
+++ app/controllers/ember_tests_controller.rb
@@ -1,9 +1,13 @@
 class EmberTestsController < ActionController::Base
   def index
-    render text: test_html_with_corrected_asset_urls, layout: false
+    render text: test_html_with_rails_assets, layout: false
   end
 
   private
+
+  def test_html_with_rails_assets
+    add_rails_assets test_html_with_corrected_asset_urls
+  end
 
   def test_html_with_corrected_asset_urls
     test_html.gsub(%r{assets/}i, "#{asset_prefix}/#{app_name}/")
@@ -27,5 +31,10 @@ class EmberTestsController < ActionController::Base
 
   def asset_prefix
     Rails.configuration.assets.prefix
+  end
+
+  def add_rails_assets(html)
+    index = html.index("<base href=\"/\" />")
+    html.insert(index, "<script src=\"/assets/application.js\"></script><link rel=\"stylesheet\" href=\"/assets/application.css\">")
   end
 end
